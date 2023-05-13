98beebbd547dcc627bba60f08dccc23ca219dd83
Yigit Polat <yigitpolatt@gmail.com>
2023-05-13 21:17:35 +0200

    Create install.sh

---
+++ homebrew/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install
+  fi
+
+fi
+
+exit 0
